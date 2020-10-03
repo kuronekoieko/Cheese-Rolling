ed60e1419b000ff26be99ba48b77c92706f4582c
家子智成 <>
2020-10-03 18:41:36 +0900

    左右移動仮実装

---
+++ Assets/0_MyAssets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] RagDollController ragDollController;
+    float dx;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        dx = 0;
+
+        if (Input.GetMouseButton(0))
+        {
+            dx = Input.GetAxis("Mouse X");
+        }
+
+        Debug.Log(dx);
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        ragDollController.AddForce(Vector3.right * dx * 500);
+    }
+}
---
+++ Assets/0_MyAssets/Scripts/Game/RagDollController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollController : MonoBehaviour
+{
+    Rigidbody[] rigidbodies;
+
+    private void Awake()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void AddForce(Vector3 force, ForceMode mode = ForceMode.Force)
+    {
+        foreach (var rigidbody in rigidbodies)
+        {
+            rigidbody.AddForce(force, mode);
+        }
+    }
+}
