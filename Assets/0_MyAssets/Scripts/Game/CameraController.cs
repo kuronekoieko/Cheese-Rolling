919d3fcd91383ab4ebce1889ffbcdd3ed93f995a
家子智成 <>
2020-10-03 18:56:30 +0900

    カメラの追従

--- Assets/0_MyAssets/Scripts/Game/CameraController.cs
+++ Assets/0_MyAssets/Scripts/Game/CameraController.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Zenject;
 
-/// <summary>
-/// Unityで解像度に合わせて画面のサイズを自動調整する
-/// http://www.project-unknown.jp/entry/2017/01/05/212837
-/// </summary>
 public class CameraController : MonoBehaviour
 {
-    public static CameraController i;
+    [Inject] PlayerController playerController;
+    Vector3 offset;
     void Start()
     {
-        if (i == null) i = this;
+        offset = transform.position - playerController.transform.position;
     }
 
     void Update()
     {
-
+        transform.position = playerController.transform.position + offset;
     }
 }
